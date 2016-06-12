9fe22f31fa1241ae64861cf655799b4635f79894
moritzz <mz@ia.net>
2016-06-12 10:31:05 +0200

    Fixed EOL format of ICS output.

--- templates/ics.php
+++ templates/ics.php
@@ -1,24 +1,24 @@
 <?php header('Content-type: text/calendar; charset=utf-8'); ?>
 <?php header("Content-Disposition: inline; filename={$page->slug()}.ics"); ?>
-<?= "BEGIN:VCALENDAR\n\r" ?>
-<?= "VERSION:2.0\n\r" ?>
-<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//" . str::upper($site->language()->code()) . "\n\r" ?>
-<?= "CALSCALE:GREGORIAN\n\r" ?>
-<?= "METHOD:PUBLISH\n\r" ?>
+<?= "BEGIN:VCALENDAR\r\n" ?>
+<?= "VERSION:2.0\r\n" ?>
+<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//" . str::upper($site->language()->code()) . "\r\n" ?>
+<?= "CALSCALE:GREGORIAN\r\n" ?>
+<?= "METHOD:PUBLISH\r\n" ?>
 <?php foreach ($page->siblings($self = false) as $key => $sibling): ?>
 <?php if ($sibling->calendar()->isNotEmpty()): ?>
 <?php $calendar = calendar($sibling->calendar()->yaml()); ?>
 <?php foreach ($calendar->getAllEvents() as $event): ?>
-<?= "BEGIN:VEVENT\n\r" ?>
-<?= "DTSTART:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
-<?= "DTEND:" . gmdate('Ymd\THis\Z', $event->getEndTimestamp()) . "\n\r" ?>
-<?= "UID:" . $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36) . "\n\r" ?>
-<?= "DTSTAMP:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
-<?= "LOCATION:" . $event->getField('location') . "\n\r" ?>
-<?= "DESCRIPTION:" . $event->getField('description') . "\n\r" ?>
-<?= "SUMMARY:" . $event->getField('summary') . "\n\r"?>
-<?= "END:VEVENT\n\r" ?>
+<?= "BEGIN:VEVENT\r\n" ?>
+<?= "DTSTART:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\r\n" ?>
+<?= "DTEND:" . gmdate('Ymd\THis\Z', $event->getEndTimestamp()) . "\r\n" ?>
+<?= "UID:" . $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36) . "\r\n" ?>
+<?= "DTSTAMP:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\r\n" ?>
+<?= "LOCATION:" . $event->getField('location') . "\r\n" ?>
+<?= "DESCRIPTION:" . $event->getField('description') . "\r\n" ?>
+<?= "SUMMARY:" . $event->getField('summary') . "\r\n"?>
+<?= "END:VEVENT\r\n" ?>
 <?php endforeach; ?>
 <?php endif ?>
 <?php endforeach; ?>
-<?= "END:VCALENDAR\n\r" ?>
+<?= "END:VCALENDAR\r\n" ?>
