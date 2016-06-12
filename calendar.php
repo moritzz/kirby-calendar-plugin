55add1112ed54e7480b2a9351d97912e76cee185
Moritz Zimmer <mz@ia.net>
2016-06-12 12:22:50 +0200

    Removed deprecated iCal snippets and added color field to the registry.

--- calendar.php
+++ calendar.php
@@ -4,12 +4,11 @@
 // = Component Registery =
 // =======================
 
+$kirby->set('field', 'color', __DIR__ . '/fields/color');
 $kirby->set('blueprint', 'event', __DIR__ . '/blueprints/event.php');
 $kirby->set('blueprint', 'ics', __DIR__ . '/blueprints/ics.php');
 $kirby->set('template', 'ics', __DIR__ . '/templates/ics.php');
 $kirby->set('snippet', 'calendar-div', __DIR__ . '/snippets/calendar-div.php');
-$kirby->set('snippet', 'calendar-ical-head', __DIR__ . '/snippets/calendar-ical-head.php');
-$kirby->set('snippet', 'calendar-ical-event', __DIR__ . '/snippets/calendar-ical-event.php');
 $kirby->set('snippet', 'calendar-table', __DIR__ . '/snippets/calendar-table.php');
 $kirby->set('snippet', 'calendar-teaser', __DIR__ . '/snippets/calendar-teaser.php');
 
