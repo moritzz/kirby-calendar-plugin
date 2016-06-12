bad6a4eeaec7a6c3b408a7f61d7de5b437d6db8f
Moritz Zimmer <mz@ia.net>
2016-06-12 12:23:39 +0200

    Work in progress on time zone support.

--- templates/ics.php
+++ templates/ics.php
@@ -1,24 +1,43 @@
 <?php header('Content-type: text/calendar; charset=utf-8'); ?>
 <?php header("Content-Disposition: inline; filename={$page->slug()}.ics"); ?>
-<?= "BEGIN:VCALENDAR\r\n" ?>
-<?= "VERSION:2.0\r\n" ?>
-<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//" . str::upper($site->language()->code()) . "\r\n" ?>
-<?= "CALSCALE:GREGORIAN\r\n" ?>
-<?= "METHOD:PUBLISH\r\n" ?>
+<?= "BEGIN:VCALENDAR\n\r" ?>
+<?= "VERSION:2.0\n\r" ?>
+<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//" . str::upper($site->language()->code()) . "\n\r" ?>
+<?= "CALSCALE:GREGORIAN\n\r" ?>
+<?= "X-APPLE-CALENDAR-COLOR:#{$page->calendar_color->or('1BADF8')}\n\r" ?>
+<?= "X-WR-TIMEZONE:Europe/Zurich\n\r" ?>
+<?= "BEGIN:VTIMEZONE\n\r" ?>
+<?= "TZID:Europe/Zurich\n\r" ?>
+<?= "BEGIN:DAYLIGHT\n\r" ?>
+<?= "TZOFFSETFROM:+0100\n\r" ?>
+<?= "RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU\n\r" ?>
+<?= "DTSTART:19810329T020000\n\r" ?>
+<?= "TZNAME:MESZ\n\r" ?>
+<?= "TZOFFSETTO:+0200\n\r" ?>
+<?= "END:DAYLIGHT\n\r" ?>
+<?= "BEGIN:STANDARD\n\r" ?>
+<?= "TZOFFSETFROM:+0200\n\r" ?>
+<?= "RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU\n\r" ?>
+<?= "DTSTART:19961027T030000\n\r" ?>
+<?= "TZNAME:MEZ\n\r" ?>
+<?= "TZOFFSETTO:+0100\n\r" ?>
+<?= "END:STANDARD\n\r" ?>
+<?= "END:VTIMEZONE\n\r" ?>
+<?= "METHOD:PUBLISH\n\r" ?>
 <?php foreach ($page->siblings($self = false) as $key => $sibling): ?>
 <?php if ($sibling->calendar()->isNotEmpty()): ?>
 <?php $calendar = calendar($sibling->calendar()->yaml()); ?>
 <?php foreach ($calendar->getAllEvents() as $event): ?>
-<?= "BEGIN:VEVENT\r\n" ?>
-<?= "DTSTART:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\r\n" ?>
-<?= "DTEND:" . gmdate('Ymd\THis\Z', $event->getEndTimestamp()) . "\r\n" ?>
-<?= "UID:" . $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36) . "\r\n" ?>
-<?= "DTSTAMP:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\r\n" ?>
-<?= "LOCATION:" . $event->getField('location') . "\r\n" ?>
-<?= "DESCRIPTION:" . $event->getField('description') . "\r\n" ?>
-<?= "SUMMARY:" . $event->getField('summary') . "\r\n"?>
-<?= "END:VEVENT\r\n" ?>
+<?= "BEGIN:VEVENT\n\r" ?>
+<?= "DTSTART:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
+<?= "DTEND:" . gmdate('Ymd\THis\Z', $event->getEndTimestamp()) . "\n\r" ?>
+<?= "UID:" . $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36) . "\n\r" ?>
+<?= "DTSTAMP:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
+<?= "LOCATION:" . $event->getField('location') . "\n\r" ?>
+<?= "DESCRIPTION:" . $event->getField('description') . "\n\r" ?>
+<?= "SUMMARY:" . $event->getField('summary') . "\n\r"?>
+<?= "END:VEVENT\n\r" ?>
 <?php endforeach; ?>
 <?php endif ?>
 <?php endforeach; ?>
-<?= "END:VCALENDAR\r\n" ?>
+<?= "END:VCALENDAR\n\r" ?>
