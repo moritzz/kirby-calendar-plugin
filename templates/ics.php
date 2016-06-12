a389970a837b6987113c1c6ecc241bcd78388247
Moritz Zimmer <mz@ia.net>
2016-06-12 10:02:11 +0200

    Changed all lines to end with new line and return.

--- templates/ics.php
+++ templates/ics.php
@@ -1,25 +1,25 @@
 <?php header('Content-type: text/calendar; charset=utf-8'); ?>
 <?php header("Content-Disposition: inline; filename={$page->slug()}.ics"); ?>
-BEGIN:VCALENDAR 
-VERSION:2.0 
-PRODID:-//<?php echo $site->url(); ?>//Kirby Calendar Plugin//<?php echo str::upper($site->language()->code()); ?> 
-CALSCALE:GREGORIAN 
-METHOD:PUBLISH 
+<?= "BEGIN:VCALENDAR\n\r" ?>
+<?= "VERSION:2.0\n\r" ?>
+<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//". str::upper($site->language()->code()); . "\n\r" ?>
+<?= "CALSCALE:GREGORIAN\n\r" ?>
+<?= "METHOD:PUBLISH\n\r" ?>
 <?php foreach ($page->siblings($self = false) as $key => $sibling): ?>
 <?php if ($sibling->calendar()->isNotEmpty()): ?>
 <?php $calendar = calendar($sibling->calendar()->yaml()); ?>
 <?php foreach ($calendar->getAllEvents() as $event): ?>
-BEGIN:VEVENT 
-DTSTART:<?php echo gmdate('Ymd\THis\Z', $event->getBeginTimestamp()); ?> 
-DTEND:<?php echo gmdate('Ymd\THis\Z', $event->getEndTimestamp()); ?> 
-UID:<?php echo $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36); ?> 
-DTSTAMP:<?php echo gmdate('Ymd\THis\Z', $event->getBeginTimestamp()); ?> 
-LOCATION:<?php echo $event->getField('location') ?> 
-DESCRIPTION:<?php echo $event->getField('description') ?> 
-SUMMARY:<?php echo $event->getField('summary') ?> 
-END:VEVENT
+<?= "BEGIN:VEVENT\n\r" ?>
+<?= "DTSTART:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
+<?= "DTEND:" . gmdate('Ymd\THis\Z', $event->getEndTimestamp()) . "\n\r" ?>
+<?= "UID:" . $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36) . "\n\r" ?>
+<?= "DTSTAMP:" gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
+<?= "LOCATION:" . $event->getField('location') . "\n\r" ?>
+<?= "DESCRIPTION:" . $event->getField('description') . "\n\r" ?>
+<?= "SUMMARY:" . $event->getField('summary') . "\n\r"?>
+<?= "END:VEVENT\n\r" ?>
 <?php endforeach; ?>
 <?php endif ?>
 <?php endforeach; ?>
-END:VCALENDAR
+<?= "END:VCALENDAR\n\r" ?>
 <?php die(); ?>
