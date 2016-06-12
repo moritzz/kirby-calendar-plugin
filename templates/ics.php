fbf29e5af6512cb8b475fbb2d4377c8486a7dbec
Moritz Zimmer <mz@ia.net>
2016-06-12 10:07:18 +0200

    Fixed typos in ICS template.

--- templates/ics.php
+++ templates/ics.php
@@ -2,7 +2,7 @@
 <?php header("Content-Disposition: inline; filename={$page->slug()}.ics"); ?>
 <?= "BEGIN:VCALENDAR\n\r" ?>
 <?= "VERSION:2.0\n\r" ?>
-<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//". str::upper($site->language()->code()); . "\n\r" ?>
+<?= "PRODID:-//" . $site->url() . "//Kirby Calendar Plugin//" . str::upper($site->language()->code()) . "\n\r" ?>
 <?= "CALSCALE:GREGORIAN\n\r" ?>
 <?= "METHOD:PUBLISH\n\r" ?>
 <?php foreach ($page->siblings($self = false) as $key => $sibling): ?>
@@ -13,7 +13,7 @@
 <?= "DTSTART:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
 <?= "DTEND:" . gmdate('Ymd\THis\Z', $event->getEndTimestamp()) . "\n\r" ?>
 <?= "UID:" . $sibling->hash() . '-' . base_convert(sprintf('%u', crc32($event->getBeginTimestamp())), 10, 36) . "\n\r" ?>
-<?= "DTSTAMP:" gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
+<?= "DTSTAMP:" . gmdate('Ymd\THis\Z', $event->getBeginTimestamp()) . "\n\r" ?>
 <?= "LOCATION:" . $event->getField('location') . "\n\r" ?>
 <?= "DESCRIPTION:" . $event->getField('description') . "\n\r" ?>
 <?= "SUMMARY:" . $event->getField('summary') . "\n\r"?>
